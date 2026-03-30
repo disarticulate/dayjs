22604ba0a6c2fd2eb24a956698d311a5c0e4705d
cy <eric@lorefolk.com>
2026-03-30 18:48:32 -0500

    build: add esm/ directory with proper ESM module support
    - Fix ESM plugin constant imports from '../../constant' to '../../constant.mjs'
    - Commit esm/ directory for third-party ESM imports
    - Remove esm/ from .gitignore

---
+++ esm/plugin/isoWeeksInYear/index.js
@@ -0,0 +1,15 @@
+export default ((o, c) => {
+  const proto = c.prototype;
+
+  proto.isoWeeksInYear = function () {
+    const isLeapYear = this.isLeapYear();
+    const last = this.endOf('y');
+    const day = last.day();
+
+    if (day === 4 || isLeapYear && day === 5) {
+      return 53;
+    }
+
+    return 52;
+  };
+});
